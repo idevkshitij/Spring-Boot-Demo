d9ac38ed9955b5cdda0d322e25374a6e368547d7
idevkshitij <mail.kshitij09@gmail,com>
2021-09-08 19:34:46 +0530

    Implemented and Tested exposed API with Get and Post requests using Postman Client.

---
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,31 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Person;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao{
+	
+	private static List<Person> DB = new ArrayList<>();
+	
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		// TODO Auto-generated method stub
+		DB.add(new Person(id, person.getName())); 
+		return 0;
+	}
+
+	@Override
+	public List<Person> selectAllPeople() {
+		// TODO Auto-generated method stub
+		return DB;
+	}
+	
+
+
+}
---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import java.beans.JavaBean;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class Person {
+
+	private final UUID id;
+	private final String name;
+	
+	public Person(@JsonProperty("id") UUID id, 
+				@JsonProperty("name") String name) {
+		
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public UUID getId() {
+		return id;
+	} 
+	
+	public String getName() {
+		return name;
+	}
+	
+}
